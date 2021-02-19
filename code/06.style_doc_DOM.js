512592030da196918a4d1b7d995c747984c05ea0
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-02-19 20:44:31 +0600

    Little modified in 06

--- code/06.style_doc_DOM.js
+++ code/06.style_doc_DOM.js
@@ -26,3 +26,16 @@ Object.assign(pTag.style, pStyle);
 
 Object.assign($('#anotherP').style, pStyle);
 // Here we are not assining into any varible/let
+
+
+
+
+
+// index.html 
+/*
+<div class="container">
+    <h1 id="h1Tag">Style Your Document</h1>
+    <p id="pTag">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis vero quod maiores deleniti quisquam fugiat ut accusantium? Ex adipisci corporis fugiat cum deleniti sed id. Fugit nisi provident natus, modi autem aspernatur quo doloribus soluta exercitationem, magni iure, perspiciatis nostrum earum aliquam corrupti cupiditate debitis ipsam nulla in eius.</p>
+    <p id="anotherP">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime exercitationem excepturi, impedit earum, enim odit eos sapiente maiores neque numquam dolore labore obcaecati doloribus delectus magni illum omnis libero incidunt!</p>
+</div>
+*/
