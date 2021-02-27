4cd5a8e797fe96d19706d9d30d7bfbe2ce59a978
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-02-27 22:31:49 +0600

    Very few changes come in DOM

--- code/05.QuerySelector_DOM.js
+++ code/05.QuerySelector_DOM.js
@@ -1,12 +1,18 @@
 /**************************
 * Bismillahir Rahmanir Rahim
 * DOM T-05
-* Query Selector of DOM
+* Query Selector in DOM
 * Author : Shihan Chowdhury
 * Thanks to - Twinkle Cats 
 **************************/
 
 let h1_id = document.querySelector('#h1-tag');
+
+// Now we are minimizing document.querySelector('#....); by making a funciton
+function $(selector) {
+    return document.querySelector(selector);
+}
+
 // let btn_id = document.querySelector('#btn-id');
 let btn_id = $('#btn-id');
 let colors = ['yellow', 'purple', 'salmon'];
@@ -23,11 +29,6 @@ btn_id.addEventListener('click', function() {
     }
 });
 
-// Now we are minimizing document.querySelector('#....); by making a funciton
-function $(selector) {
-    return document.querySelector(selector);
-}
-
 let btnId2 = $('#btn-id-2');
 let pTag = $('#pTag');
 btnId2.addEventListener('click', function(){
@@ -40,7 +41,7 @@ btnId2.addEventListener('click', function(){
 /* <div class="container">
 <div class="row">
     <div class="col">
-        <h1 class="text-center" id = "h1-tag">04 Basic Selector in DOM</h1>
+        <h1 class="text-center" id = "h1-tag">04 Query Selector in DOM</h1>
         <p id="pTag">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium iusto accusamus dolor. Fuga, ex dolore? Quibusdam corrupti vero debitis non repellat voluptate vel culpa aspernatur?</p>
         <button id="btn-id" class="btn btn-primary">Click Me</button>
         <button id="btn-id-2" class="btn btn-success">Click Me</button>
